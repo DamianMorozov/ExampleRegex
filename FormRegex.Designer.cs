2d54430b5c228f359cc4dce51d160ebe95f31631
Damian Morozov <support@morozov33.ru>
2017-06-03 00:41:44 +0300

    0.1.1
    Первый релиз.
    Есть поиск.

---
+++ FormRegex.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace StudingRegex
+{
+    partial class FormRegex
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonMatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(150, 7);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(461, 26);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(150, 37);
+            this.textBoxFind.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(461, 26);
+            this.textBoxFind.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Входная строка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Строка поиска";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(150, 70);
+            this.textBoxResult.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(461, 26);
+            this.textBoxResult.TabIndex = 4;
+            // 
+            // buttonMatch
+            // 
+            this.buttonMatch.Location = new System.Drawing.Point(10, 70);
+            this.buttonMatch.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonMatch.Name = "buttonMatch";
+            this.buttonMatch.Size = new System.Drawing.Size(127, 26);
+            this.buttonMatch.TabIndex = 5;
+            this.buttonMatch.Text = "Поиск";
+            this.buttonMatch.UseVisualStyleBackColor = true;
+            this.buttonMatch.Click += new System.EventHandler(this.buttonMatch_Click);
+            // 
+            // FormRegex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.buttonMatch);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.textBoxInput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormRegex";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Регулярные выражения";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonMatch;
+    }
+}
+
---
+++ FormRegex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace StudingRegex
+{
+    public partial class FormRegex : Form
+    {
+        public FormRegex()
+        {
+            InitializeComponent();
+        }
+
+        // Поиск подстроки
+        private void buttonMatch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Regex.IsMatch(textBoxInput.Text, textBoxFind.Text))
+                    textBoxResult.Text = @"Найдено в позиции " + Regex.Match(textBoxInput.Text, textBoxFind.Text).Index;
+                else
+                    textBoxResult.Text = @"Не найдено";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(@"Ошибка поиска!" + Environment.NewLine + exception.Message);
+            }
+        }
+    }
+}
